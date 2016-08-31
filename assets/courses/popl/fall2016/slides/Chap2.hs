33f74ab7e1748d752cb5cbbde97da290b9ccd982
William Harrison <bill@mwc-065110.dhcp.missouri.edu>
2016-08-31 16:00:59 -0500

    incremental

--- assets/courses/popl/fall2016/slides/Chap2.hs
+++ assets/courses/popl/fall2016/slides/Chap2.hs
@@ -1,4 +1,19 @@
 module Chap2 where
 
+import Prelude hiding (head,tail,(!!))
+
 doubleMe x = x + x  
+
+head :: [a] -> a
+head []     = error "Hey Stupid it's empty"
+head (x:xs) = x
+
+tail :: [a] -> [a]
+tail []     = undefined
+tail (x:xs) = xs
+
+-- (!!) :: [a] -> Int -> a
+[] !! n     = undefined
+(x:xs) !! n = if n==0 then x else xs !! (n-1)
+
 
